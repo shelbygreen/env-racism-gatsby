bb29e99dbc61df4eeb3dac3883c2248a131682b6
Shelby Green <shelbygreen@Shelbys-MacBook-Pro.local>
2020-10-26 10:44:08 -0400

    testing landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 import AboutSection from "./About"
 
 const IndexPage = () => (
-  <div>
+  <Layout>
     <SEO title="Home" />
     <AboutSection />
-  </div>
+  </Layout>
 )
 
 export default IndexPage
