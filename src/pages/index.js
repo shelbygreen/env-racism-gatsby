8a2395bdcdca90bb004a7da02385f0d74a0e5261
Shelby Green <shelbygreen@Shelbys-MacBook-Pro.local>
2020-10-26 10:54:49 -0400

    testing landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,23 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
-import { Box, Flex } from '../components/Grid'
-import { Button } from '../components/Button'
-import { Link } from '../components/Link'
-import styled, { themeGet } from '../../util/style'
-import { Section, Title } from './styles'
-
-const Columns = styled(Flex).attrs({
-  flexWrap: 'wrap',
-  justifyContent: 'space-between',
-})``
-
 const IndexPage = () => (
-  <Section>
+  <Layout>
     <SEO title="Home" />
-    <Title><center>Texas Environmental Justice Explorer</center></Title>
-        <p>
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-        quae ab illo inventore veritatis et quasi architecto beatae vitae
-        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-        eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-        sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-        commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate
-        velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat
-        quo voluptas nulla pariatur?"
-        </p>
-        <center>
-        <Button>
-          <Link to="/page-2/">Enter</Link>
-        </Button>
-        </center>
-  </Section>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/About">Go to page 2</Link>
+  </Layout>
 )
 
 export default IndexPage
