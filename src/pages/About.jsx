55402101c5e22c5a9cfa1ad0bada84fc2f52298d
Shelby Green <shelbygreen@Shelbys-MacBook-Pro.local>
2020-10-26 10:38:38 -0400

    testing landing page

---
+++ src/pages/About.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { Box, Flex } from '../components/Grid'
+import { Button } from '../components/Button'
+import { Link } from '../components/Link'
+import styled, { themeGet } from '../../util/style'
+import { Section, Title } from './styles'
+
+const Columns = styled(Flex).attrs({
+  flexWrap: 'wrap',
+  justifyContent: 'space-between',
+})``
+
+
+const About = () => (
+  <Section>
+    <Title><center>Texas Environmental Justice Explorer</center></Title>
+        <p>
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+        quae ab illo inventore veritatis et quasi architecto beatae vitae
+        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+        eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+        sed quia non numquam eius modi tempora incidunt ut labore et dolore
+        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+        commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate
+        velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat
+        quo voluptas nulla pariatur?"
+        </p>
+        <center>
+        <Button>
+          <Link to="/explore">Enter</Link>
+        </Button>
+        </center>
+  </Section>
+)
+
+export default About
